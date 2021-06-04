66125d8cb7c75c6ef26d0989e8c071a5dbf295e8
Nahidjc <nahidjc80@gmail.com>
2021-06-04 23:42:05 +0600

    sidebar starting

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Button, Input } from 'reactstrap';
 
 class Sidebar extends Component {
     render() {
         return (
-            <div>
-                <h1>This Sidebar </h1>
+            <div style={{background:'#efefef',padding:'10px'}}>
+            
+               <div className='d-flex mb-5'>
+               <Input 
+                   type='search'
+                   placeholder='Search'
+                   value={this.props.searchTerm}
+                   onChange={e => this.props.handleSearch(e.target.value)}
+               />
+               <Button
+               
+               color='success'
+               className='ml-2'
+               onClick={this.toggleModal}
+               >
+                   New
+               </Button>
+
+             
+
+               </div>
             </div>
         );
     }
--- src/data/polls.js
+++ src/data/polls.js
@@ -1,16 +1,14 @@
-const polls =[
-    {
-        id:'CC749',
-        title:'What is your favourite programming language?',
-        description:'there are lot of popular programming languages available.Among them what is your favourite',
-        option:[
-            {id:'1a' , value:'C programming' ,vote:0},
-            {id:'2a' , value:'Java' ,vote:0},
-            {id:'3a' , value:'JavaScript' ,vote:0},
-            {id:'4a' , value:'Python' ,vote:0}
-        ],
-        created:new Date(),
-        totalVote:0,
-        opinions:[]
-    }
-]
+const polls = [{
+    id: 'CC749',
+    title: 'What is your favourite programming language?',
+    description: 'there are lot of popular programming languages available.Among them what is your favourite',
+    option: [
+        { id: '1a', value: 'C programming', vote: 0 },
+        { id: '2a', value: 'Java', vote: 0 },
+        { id: '3a', value: 'JavaScript', vote: 0 },
+        { id: '4a', value: 'Python', vote: 0 }
+    ],
+    created: new Date(),
+    totalVote: 0,
+    opinions: []
+}]
