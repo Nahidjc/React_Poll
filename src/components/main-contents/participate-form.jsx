c64868af785c9c26e16198dfa3510d9d6a20e56f
Nahidjc <nahidjc80@gmail.com>
2021-06-08 15:17:06 +0600

    simple participation form adding

--- src/components/main-contents/participate-form.jsx
+++ src/components/main-contents/participate-form.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button, CustomInput, Form, FormFeedback, FormGroup, Input, Label } from 'reactstrap'
 
 class ParticipationForm extends Component {
     state = {
@@ -53,9 +54,88 @@ class ParticipationForm extends Component {
 
     render() {
         return (
-            <div>
+            <Form onSubmit={this.handleSubmit}>
+                <div className="d-flex">
+                    <h4>Options</h4>
+                    <Button
+                        color='warning'
+                        type='button'
+                        className='ml-auto'
+                        onClick={this.props.toggleModal}
+                    >
+                        Edit
+                   </Button>
 
-            </div>
+                    <Button
+                        type='button'
+                        className='ml-auto'
+                        onClick={() => this.props.deletePoll(this.props.poll.id)}
+                    >
+                        Delete
+
+                   </Button>
+                </div>
+                {
+
+                    this.props.poll.options.map(opt => (
+                        <FormGroup className='my-2' key={opt.id}>
+                            <Label className='d-flex'>
+                                <CustomInput
+                                    type='radio'
+                                    id={opt.id}
+                                    name='selectedOption'
+                                    value={opt.id}
+                                    onChange={this.handleChange}
+                                    invalid={this.state.errors.selectedOption ? true : false}
+                                />
+                                {opt.value}
+                                <span
+                                    style={{ padding: '5px 20px', background: 'green', color: 'white', borderRadius: '5px' }}
+                                    className='ml-auto'
+
+                                >{opt.vote}</span>
+
+                                <span
+
+                                    style={{
+                                        padding: '5px 20px',
+                                        background: 'orange',
+                                        color: 'white',
+                                        borderRadius: '5px'
+                                    }}
+                                    className='ml-2'
+                                >
+                                    {this.poll.totalVote > 0
+
+                                        ? (
+                                            (100 * opt.vote) /
+                                            this.props.poll.totalVote
+                                        ).toFixed(2) : 0
+                                    }
+                                </span>
+
+                            </Label>
+
+                        </FormGroup>
+                    ))
+                }
+                <FormGroup className='my-3'>
+                    <Label>
+                        Enter Your Name
+                    </Label>
+                    <Input
+                        name='name'
+                        placeholder='Nahid Hasan'
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        invalid={this.state.errors.name ? true : false}
+                    />
+                    {this.state.errors.name && <FormFeedback>{this.state.name}</FormFeedback>}
+
+                </FormGroup>
+                <Button type='submit'>Submit Your Opinion</Button>
+
+            </Form>
         )
     }
 }
